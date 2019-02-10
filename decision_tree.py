016d92affb50bc56cba9dee838c0f98ed81a4109
Jacob Meyers <jmeyers35@gatech.edu>
2019-02-10 17:40:43 -0500

    remove commented code

--- decision_tree.py
+++ decision_tree.py
@@ -51,72 +51,7 @@ def plot_wine():
     print("Accuracy: {}".format(accuracy))
     print("Confusion Matrix: \n", confusion_matrix_wine)
     plt.show()
-
-
-# def plot_flowers():
-#     flower_X, flower_y = loadFlowerData()
-
-#     train_sizes, train_scores, valid_scores, y_test, y_pred = run_decision_tree(flower_X, flower_y)
-
-#     plt.figure()
-#     plt.title("Pruned Decision Tree Learning Curves: Iris Data")
-#     plt.xlabel("Training examples")
-#     plt.ylabel("Score")
-#     train_scores_mean = np.mean(train_scores, axis=1)
-#     train_scores_std = np.std(train_scores, axis=1)
-#     test_scores_mean = np.mean(valid_scores, axis=1)
-#     test_scores_std = np.std(valid_scores, axis=1)
-#     plt.grid()
-#     plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-#                      train_scores_mean + train_scores_std, alpha=0.1,
-#                      color="r")
-#     plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-#                      test_scores_mean + test_scores_std, alpha=0.1, color="g")
-#     plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-#              label="Training score")
-#     plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-#              label="Cross-validation score")
-
-#     plt.legend(loc="best")
-#     accuracy = accuracy_score(y_test, y_pred)
-#     confusion_matrix_wine = confusion_matrix(y_test, y_pred)
-#     print("Accuracy: {}%".format(accuracy))
-#     print("Confusion Matrix: \n", confusion_matrix_wine)
-#     plt.show()
-
-# def plot_heart():
-#     heart_X, heart_y = loadHeart()
-
-#     train_sizes, train_scores, valid_scores, y_test, y_pred = run_decision_tree(heart_X, heart_y)
-
-#     plt.figure()
-#     plt.title("Pruned Decision Tree Learning Curves: Heart Disease Data")
-#     plt.xlabel("Training examples")
-#     plt.ylabel("Score")
-#     train_scores_mean = np.mean(train_scores, axis=1)
-#     train_scores_std = np.std(train_scores, axis=1)
-#     test_scores_mean = np.mean(valid_scores, axis=1)
-#     test_scores_std = np.std(valid_scores, axis=1)
-#     plt.grid()
-#     plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-#                      train_scores_mean + train_scores_std, alpha=0.1,
-#                      color="r")
-#     plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-#                      test_scores_mean + test_scores_std, alpha=0.1, color="g")
-#     plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-#              label="Training score")
-#     plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-#              label="Cross-validation score")
-
-#     plt.legend(loc="best")
-#     accuracy = accuracy_score(y_test, y_pred)
-#     confusion_matrix_wine = confusion_matrix(y_test, y_pred)
-#     print("Accuracy: {}%".format(accuracy))
-#     print("Confusion Matrix: \n", confusion_matrix_wine)
-#     plt.show()
-
-
-
+    
 def plot_car():
     car_X, car_y, finalCarX, finalCarY = loadCar()
     train_sizes, train_scores, valid_scores, y_test, y_pred = run_decision_tree(car_X, car_y,finalCarX, finalCarY)
